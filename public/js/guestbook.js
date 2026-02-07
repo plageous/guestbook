e4ad3b41a08fdc0b0d0430bcdddf9b1fff60a4a2
KJ <kjmiclea@gmail.com>
2026-02-07 14:23:17 -0800

    Added significant additions to guestbook.js

--- public/js/guestbook.js
+++ public/js/guestbook.js
@@ -1,1 +1,54 @@
-// input validation? that wasn't on the rubric...
+document.getElementById("contact-form").onsubmit = () => {
+    clearErrors();
+    let isValid = true;
+
+    let fname = document.getElementById("fname").value.trim();
+    let lname = document.getElementById("lname").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let linkedin = document.getElementById("linkedin").value.trim();
+
+    let how_we_met = document.getElementById("meeting").value;
+
+    // first name
+    if (!fname) {
+        document.getElementById("err-fname").style.display = "block";
+        isValid = false;
+    }
+
+    // last name
+    if (!lname) {
+        document.getElementById("err-lname").style.display = "block";
+        isValid = false;
+    }
+
+    // email
+    if (!email.includes("@") && !email.includes(".")) {
+        document.getElementById("err-email").style.display = "block";
+        isValid = false;
+    }
+
+    // linkedin
+    if (!linkedin.includes("https://linkedin.com/in/" && linkedin)) {
+        document.getElementById("err-linkedin").style.display = "block";
+        isValid = false;
+    }
+
+
+    // how we met
+    if (how_we_met == "none") {
+        document.getElementById("err-how-we-met").style.display = "block";
+        isValid = false;
+    }
+
+    if (!isValid) {
+        window.scrollTo({top: 0, left: 0, behavior:'smooth'});
+    }
+    return isValid;
+}
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("err")
+    for (let i = 0; i < errors.length; i++) {
+        errors[i].style.display = "none";
+    }
+}
